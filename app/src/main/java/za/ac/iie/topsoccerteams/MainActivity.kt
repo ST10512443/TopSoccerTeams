9b3aa5604131fb6621ede83003b601fd8711d8bb
ST10512443 <ST10512443@myemeris.edu.za>
2026-04-09 16:08:47 +0200

    Displaying array content in textview

--- app/src/main/java/za/ac/iie/topsoccerteams/MainActivity.kt
+++ app/src/main/java/za/ac/iie/topsoccerteams/MainActivity.kt
@@ -1,21 +1,32 @@
 package za.ac.iie.topsoccerteams
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Arrays
 
 class MainActivity : AppCompatActivity() {
+    //created array to holf top 5 soccer teams
     val teams = arrayOf<String>("Mam Sundowns FC","Orlando Pirates","Bidvest wits",
         "Stellenbosch FC","Sek United FC")
-    
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val teamsTxt = findViewById<TextView>(R.id.TeamsDisplayID)
+
+        //reassigning position 0 of the teams array
+        teams[0] = "Mam Sundowns FC :)"
+        //displaying top team in text view
+        teamsTxt.text = Arrays.toString(teams)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
