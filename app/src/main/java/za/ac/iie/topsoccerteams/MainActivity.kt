d32d696447612523efc0fa367a5abff6011b55f4
ST10512443 <ST10512443@myemeris.edu.za>
2026-04-09 15:57:32 +0200

    created array to hold teams

--- app/src/main/java/za/ac/iie/topsoccerteams/MainActivity.kt
+++ app/src/main/java/za/ac/iie/topsoccerteams/MainActivity.kt
@@ -7,6 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val teams = arrayOf<String>("Mam Sundowns FC","Orlando Pirates","Bidvest wits",
+        "Stellenbosch FC","Sek United FC")
+    
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
